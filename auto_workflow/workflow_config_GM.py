cb4764f136ef4fd1273d0887368e7aa03968ba6b
mit2nil <mit2nil@gmail.com>
2017-02-02 10:02:30 -0800

    Removed classifications from GM config

--- auto_workflow/workflow_config_GM.py
+++ auto_workflow/workflow_config_GM.py
@@ -23,17 +23,6 @@ REPO_CONFIG = [
     },
     {
         'path': repo_path,
-        'name': 'Classifications',
-        'base_uri': base_uri,
-        'rdf_root_uri': 'http://www.cidoc-crm.org/cidoc-crm/E22_Man-Made_Object1',
-        'context_uri': context_uri,
-        'model_file': 'Classifications-model.ttl',
-        'input_file': 'Classifications.csv',
-        'input_file_type': 'csv',
-        'output_file_name': 'Classifications'
-    },
-    {
-        'path': repo_path,
         'name': 'Constituents_institutions',
         'base_uri': base_uri,
         'rdf_root_uri': 'http://www.cidoc-crm.org/cidoc-crm/E39_Actor1',
